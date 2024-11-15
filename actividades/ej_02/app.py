11e98c25fe34441a264d0fabcd3a40bcdf78891d
vitaleluca <85516318+vitaleluca@users.noreply.github.com>
2024-11-15 14:04:35 -0300

    feat: subiendo actividad 2

--- actividades/ej_02/app.py
+++ actividades/ej_02/app.py
@@ -1,1 +1,37 @@
 # Aplicacion del servidor
+from microdot import Microdot, send_file
+from machine import Pin
+
+app = Microdot()
+
+led1 = Pin(32, Pin.OUT)
+led2 = Pin(33, Pin.OUT)
+led3 = Pin(25, Pin.OUT)
+
+@app.route('/')
+async def index(request):
+    return send_file('index.html')
+
+@app.route("/<dir>/<file>")
+async def static(request, dir, file):
+    return send_file("/"+ dir +"/" + file)
+
+@app.route("/led/toggle/<int:led>")
+async def LED(request, led):
+    global led1
+    if led == 1:
+        led1.value(not led1.value())
+    if led == 2:
+        led2.value(not led2.value())
+    if led == 3:
+        led3.value(not led3.value())
+
+    return { "status" : "ok" }
+
+    
+
+
+app.run(port = 80)
+
+@app.route("<dir>/<file>")
+async def static
--- actividades/ej_02/boot.py
+++ actividades/ej_02/boot.py
@@ -1,1 +1,13 @@
 # Configuracion inicial
+def do_connect():
+    import network
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print("Connecting to network...")
+        sta_if.active(True)
+        sta_if.connect("Cooperadora Alumnos", "")
+        while not sta_if.isconnected():
+            print(".",end="")
+    print("Network config:", sta_if.ifconfig())
+
+do_connect()
